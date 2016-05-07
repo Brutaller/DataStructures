84e93add241570c0b88266693555c4ed1abdf234
Brutaller <kazkachi1@gmail.com>
2016-05-07 19:02:39 +0300

    red-black trees

---
+++ src/RedBlackNode.java
@@ -0,0 +1,27 @@
+public class RedBlackNode<T extends Comparable<T>> {
+
+    public static final int BLACK = 0;
+    public static final int RED = 1;
+    public T key;
+
+    RedBlackNode<T> parent;
+    RedBlackNode<T> left;
+    RedBlackNode<T> right;
+    public int numLeft = 0;
+    public int numRight = 0;
+    public int color;
+
+    RedBlackNode(){
+        color = BLACK;
+        numLeft = 0;
+        numRight = 0;
+        parent = null;
+        left = null;
+        right = null;
+    }
+
+    RedBlackNode(T key){
+        this();
+        this.key = key;
+    }
+}
